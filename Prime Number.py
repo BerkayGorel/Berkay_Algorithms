3ed075bad547aa8bef61057d3b70d9cd09660cda
BerkayGorel <berkaygorel10@gmail.com>
2024-01-23 00:12:51 +0300

    Update Prime Number.py

--- Prime Number.py
+++ Prime Number.py
@@ -1,15 +1,19 @@
-        ##   SAYININ ASAL OLUP OLMADIĞINI GÖSTEREN ALGORİTMA
+                                        ##  CHECKKING THE ENTERED NUMBER WHETHER PRIME OR NOT
 
-count = 1
-
-number = int(input("Bir doğal sayı giriniz "))
-
-while count < number :
-    count +=1
-    if number % count == 0 :
+while True:
+    count = 1
+    
+    number = int(input("Bir doğal sayı giriniz "))
+    if number % 2 == 0 :
         print("Sayınız asal DEĞİL")
-        break
-    else:
-        print("Sayınız asal")
-        break
+        continue
+    
+    while count < number:
+        count +=2
+        if number % count == 0 :
+            print("Sayınız asal DEĞİL")
+            break
+        else:
+            print("Sayınız asal")
+            break
     
