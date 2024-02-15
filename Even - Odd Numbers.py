9b1a8d4a8f34160c3d2270bde2133a350e6f33e8
BerkayGorel <berkaygorel10@gmail.com>
2024-02-15 17:41:03 +0300

    Basic / Even - Odd Numbers.py

--- Even - Odd Numbers.py
+++ Even - Odd Numbers.py
@@ -1,35 +1,26 @@
     ##   TEK ve ÇİFT SAYILAR
 
 x = int(input("Bir tam sayı giriniz "))
-
 bosListe1= []
 bosListe2= []
 
 if x >= 0 :
-    print("Tek sayılar: ")
-    for i in range(1,x+1):
-        if i % 2 == 1 :
-            bosListe1.append(i)
-    print(bosListe1)
+    for i in range(1,x+1,2):
+        bosListe1.append(i)
+    print("Tek Sayılar:",bosListe1)
+    
+    for i in range(0,x+1,2):
+        bosListe2.append(i)
+    print("Çift Sayılar:",bosListe2)
+    
+else:
+    for i in range(x+1,0,2):
+        bosListe1.append(i)
+    print("Tek Sayılar:",bosListe1)
 
-    print("Çift sayılar: ")            
-    for i in range(1,x+1):
-        if i % 2 == 0 :
-            bosListe2.append(i)
-    print(bosListe2)
-
-if x < 0 :
-    print("Tek sayılar: ")
-    for i in range(x+1,1):
-        if i % 2 == 1 :
-            bosListe1.append(i)
-    print(bosListe1)
-
-    print("Çift sayılar: ")            
-    for i in range(x+1,1):
-        if i % 2 == 0 :
-            bosListe2.append(i)
-    print(bosListe2)
+    for i in range(x+1,1,2):
+        bosListe2.append(i)
+    print("Çift Sayılar:",bosListe2)
     
 
 
