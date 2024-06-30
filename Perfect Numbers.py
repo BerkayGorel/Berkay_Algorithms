f9eac3d49e5bf980579e537c7a9d025c2546b1cc
BerkayGorel <berkaygorel10@gmail.com>
2024-06-30 23:25:55 +0300

    Perfect Numbers.py

--- Perfect Numbers.py
+++ Perfect Numbers.py
@@ -2,26 +2,28 @@
                                                                                          ####  PERFECT NUMBERS
 
         ## Perfect Numbers
-# Firstly, I'd better create the algorithm of factors 
-while True:
-    num = int(input("Type a positive integer "))
-    fac_list = [ ]
-    for i in range(1,num):
-        if num % i == 0 :
-            fac_list.append(i)
+# Firstly, I'd better create the algorithm of divisors
+divisors = []
 
-# Then I question the sum of them (except number) are equal to the number or are not
-    add = 0 
-    for item in fac_list :
-        add = add+item
-    
-    print("summation of factors except",num, "= ",add)
-    if add == num :
-        print("Your number is defined 'perfect number' ")
+num = int(input("Type the number: "))
 
-    else:
-        print("Your number is NOT a 'perfect number' ")
+N = int(num**(1/2))
+summa = 0
+for i in range(1,N+1):
+    if num % i == 0:
+        divisors.append(int(i))
+        divisors.append(int(num/i))
+for item in divisors:
+    summa+=divisors
+if summa == num:
+    print(num,"is a perfect number")
+else:
+    print(num,"is not a perfect number")
     
+    
+      
+        
+
 
 
     
